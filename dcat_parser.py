0b43d6436c31b42e73025f3967b24ae1baa918c3
Jay Miller <kjaymiller@gmail.com>
2020-11-19 18:30:54 -0800

    add notebook

--- dcat_parser.py
+++ dcat_parser.py
@@ -1,70 +1,37 @@
 """Load JSON File and Load the Data into Elasticsearch"""
 
-import json
-from connection import client
-from elasticsearch.helpers import bulk
 import hashlib
+import json
 import typing
-import pathlib
-import os
-from markdown2 import markdown
-import itertools
 
-es_index = os.environ.get('ES_INDEX')
-input_file = 'data.json'
+from elasticsearch.helpers import bulk
+from connection import client
 
-## TODO: Add Descriptions to Posts
+es_index = os.environ.get("ES_INDEX")
+input_file = "data.json"
 
-def set_descriptions(dataset, working_dir):
-    basename = dataset['identifier']
-    working_directory = pathlib.Path(working_dir)
-
-    if (identifier:=working_directory.join(basename).with_suffix('.md')).exists():
-        return identifies.read_text()
-    
-    elif (identifier:=working_directory.join(basename[:4]).with_suffix('.md')).exists():
-        return identifies.read_text()
-
-    else:
-        return ''
-
-
-def strip_table_from_markdown(markdown_text):
-    lines = reversed(markdown_text.splitlines(keepends=True))
-    main_text = list(itertools.takewhile(lambda x: not x.strip().startswith('--'), lines))
-    print(''.join(main_text[::-1]))
-
-
-def set_city_region(dataset:str, *, city:str, region:str):
-    """Append the city and region to your data"""
-    dataset['city'] = city
-    dataset['region'] = region
-
-
-def gen_id(datasets: typing.List, *, city:str, region:str):
+def gen_id(*args):
     """Create an unique identifier based on the identifier and contactPoint
     of the dcat data
     """
-    _id = dataset['contactPoint']['fn'] + dataset['identifier']
-    dataset['_id'] = hashlib.sha1(_id.encode('utf-8')).hexdigest()
+    return hashlib.sha1(''.join(args).encode("utf-8")).hexdigest()
 
-def modify_data(dataset, **kwargs):
+
+def modify_dataset(dataset):
     for dataset in datasets:
+        dataset['_id'] = gen_id(dataset['contactPoint']['fn'], dataset['identifier'])
+        dataset["city"] = city
+        dataset["region"] = region
+        yield dataset
+        
 
-        gen_id() 
-        yield new_data
 
-
-def parse(city:str , region:str):
+def upload(dataset: typing.Sequence, *args, **kwargs):
     """Set the region to <State>, Country Abbreviation"""
 
     with open(input_file) as json_file:
         bulk(
             client=client,
             index=es_index,
-            actions=modify_data(json.load(json_file)['dataset'], city, region),
-            )
-
-
-if __name__ == '__main__':
-    parse()
+            actions=modify_dataset(json.load(json_file)["dataset"], *kwargs, **kwargs),
+        )
